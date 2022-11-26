bea603f4c3d8b10fdba3b394817f0c45ea73ca9e
Nico <penchemel.nicolas@gmail.com>
2022-11-25 19:11:29 -0500

    init

---
+++ Facture.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo_POO_6
+{
+    public class Facture
+    {
+        List<string> item;
+        List<double> prix;
+        double taxe;
+
+        public Facture(double taxe)
+        {
+            this.taxe = taxe;
+            this.item = new List<string>();
+            this.prix = new List<double>();
+        }
+
+        public void AjouterItem(string nom, double prix)
+        {
+            this.item.Add(nom);
+            this.prix.Add(prix);
+        }
+        
+        public double SousTotalHTC()
+        {
+            double totalHTC = 0.0;
+
+            for (int i = 0; i < this.prix.Count; i++)
+            {
+                totalHTC = totalHTC + this.prix[i];
+            }
+            return totalHTC;
+        }
+        public double CalculerTaxes()
+        {
+            double totalTaxe;
+
+            totalTaxe = SousTotalHTC() * this.taxe;
+
+            return totalTaxe;
+        }
+        public double TotalFacture()
+        {
+            double totalFacture = 0.0;
+
+            totalFacture = SousTotalHTC() + CalculerTaxes();
+            
+            return totalFacture;
+        }
+        public void afficheFacture()
+        {
+            for (int i = 0; i < this.prix.Count; i++);
+            {
+                Console.WriteLine("{0} : {1}", this.item[i], this.prix{i});
+            }
+            
+            Console.WriteLine("Sous Total est : {0}", SousTotalHTC());
+
+            Console.WriteLine("Taxe : {0}", CalculerTaxes());
+
+            Console.WriteLine("Total : {0]", TotalFacture());
+            
+
+        }   
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace exo_POO_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void programmeFacture()
+            {
+                Facture facture = new Facture(0.14975);
+                int choix = 1;
+
+                while (choix < 3 && choix > 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("1. Entrer item\n 2. Imprimer facture\n 3. Quitter");
+                    // Quand on rentre autre chose qu'un int avec int.TryParse
+                    // La méthode retourne faux, sinon vrai
+                    if (!int.TryParse(Console.ReadLine(), out choix))
+                    {
+                        Console.WriteLine("Recommencer l'opération, choix invalide");
+                    }
+
+                    if (choix == 1)
+                    {
+                        string nom = "";
+                        double prix = 0.0;
+                        Console.WriteLine("Entrer le nom de l'item");
+                        nom = Console.ReadLine();
+                        Console.WriteLine("Entrer le prix de l'item");
+                        double.TryParse(Console.ReadLine(), out prix);
+
+                        facture.ajouterItem(nom, prix);
+                    }
+                    else if (choix == 2)
+                    {
+                        facture.afficherFacture();
+                    }
+                    else if (choix == 3)
+                    {
+                        // quand je veux quitter mettre un nombre qui fait arrêter la boucle
+                        // dans choix
+                        choix = 0;
+                    }
+                    else
+                    {
+                        // pour ne pas sortir de la boucle quand le choix n'est pas bon
+                        // remettre choix à 1
+                        choix = 1;
+                    }
+
+                    Console.WriteLine("Appuyer sur une touche pour continuer");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
